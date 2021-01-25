e5400ce63b9e82413ea29092bd2f5f344e0bfce7
Roguez84 <t.roguez984@gmail.com>
2021-01-25 15:02:16 -0300

    Modelo fisico terminado

--- prueba_db.sql
+++ prueba_db.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE prueba1;
-\c prueba1
+CREATE DATABASE prueba;
+\c prueba;
 
 CREATE TABLE info_cliente(
-    id_cliente SERIAL,
+    ID_cli SERIAL,
     RUT INT, -- Consider putting UNIQUE
     nombre  VARCHAR(60),
     direccion VARCHAR(90),
-    PRIMARY KEY (id_cliente)
+    PRIMARY KEY (ID_cli)
 );
 
 INSERT INTO info_cliente (RUT, nombre, direccion)
@@ -27,10 +27,10 @@ VALUES (161112226,'Sergio','Camino a Peru 16');
 
 
 CREATE TABLE categoria_productos(
-    id_categoria SERIAL,
+    ID_cat SERIAL,
     nombre_categoria VARCHAR(30),
     descripcion_categoria VARCHAR(120),
-    PRIMARY KEY (id_categoria)
+    PRIMARY KEY (ID_cat)
 );
 
 INSERT INTO categoria_productos (nombre_categoria, descripcion_categoria)
@@ -44,13 +44,13 @@ VALUES ('Currency Coin', 'Monedas utilizadas para compras');
 
 
 CREATE TABLE productos(
-    id_producto SERIAL,
+    ID_pro SERIAL,
     nombre_producto VARCHAR(30), --Considerar hacer que esto sea UNIQUE
     descripcion_producto VARCHAR(120),
     valor_unitario INT,
     id_categoria INT,
-    PRIMARY KEY (id_producto),
-    FOREIGN KEY (id_categoria) REFERENCES categoria_productos(id_categoria)
+    PRIMARY KEY (ID_pro),
+    FOREIGN KEY (id_categoria) REFERENCES categoria_productos(ID_cat)
 );
 
 INSERT INTO productos (nombre_producto, descripcion_producto, valor_unitario,id_categoria)
@@ -80,11 +80,11 @@ VALUES ('LTC', 'Litecoin', 142, 3);
 
 
 CREATE TABLE factura(
-    id_factura SERIAL,
+    ID_fac SERIAL,
     id_cliente INT,
     fecha DATE, --Consider NOT NULL
-    PRIMARY KEY (id_factura),
-    FOREIGN KEY (id_cliente) REFERENCES info_cliente(id_cliente)
+    PRIMARY KEY (ID_fac),
+    FOREIGN KEY (id_cliente) REFERENCES info_cliente(ID_cli)
 );
 
 INSERT INTO factura (id_cliente, fecha)
@@ -123,67 +123,140 @@ CREATE TABLE cantidades(
     id_factura INT,
     id_producto INT,
     cantidad INT,
-    FOREIGN KEY (id_factura) REFERENCES factura(id_factura),
-    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+    FOREIGN KEY (id_factura) REFERENCES factura(ID_fac),
+    FOREIGN KEY (id_producto) REFERENCES productos(ID_pro)
 );
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (1, 6, 1000);
+VALUES (1, 5, 1);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (1, 2, 1000);
+VALUES (1, 2, 2);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (2, 1, 1000);
+VALUES (2, 1, 1);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (2, 3, 1000);
+VALUES (2, 3, 3);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (3, 8, 1000);
+VALUES (3, 8, 2);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (3, 5, 1000);
+VALUES (3, 5, 1);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (4, 4, 1000);
+VALUES (4, 4, 2);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (4, 3, 1000);
+VALUES (4, 3, 1);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (5, 7, 1000);
+VALUES (5, 7, 3);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (5, 6, 1000);
+VALUES (5, 6, 2);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (6, 3, 1000);
+VALUES (6, 3, 4);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (6, 8, 1000);
+VALUES (6, 8, 3);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (7, 4, 1000);
+VALUES (7, 4, 2);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (7, 5, 1000);
+VALUES (7, 5, 1);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (8, 1, 1000);
+VALUES (8, 1, 2);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (8, 6, 1000);
+VALUES (8, 3, 3);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (9, 8, 1000);
+VALUES (9, 8, 2);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (9, 2, 1000);
+VALUES (9, 2, 1);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (10, 1, 1000);
+VALUES (10, 1, 5);
 
 INSERT INTO cantidades(id_factura, id_producto, cantidad)
-VALUES (10, 4, 1000);
+VALUES (10, 4, 3);
 
+--Consultas
+--tabla general para mi referencia
+SELECT id_fac, fecha, id_cli, nombre, ID_pro, nombre_producto, valor_unitario, cantidad, valor_unitario*cantidad as valor_total
+FROM ((
+        SELECT *
+        FROM info_cliente i JOIN factura f ON i.ID_cli = f.id_cliente
+    )  AS cliente_factura
+    JOIN cantidades ON cantidades.id_factura = cliente_factura.ID_fac ) AS cliente_factura_cantidad
+    JOIN productos ON productos.ID_pro = cliente_factura_cantidad.id_producto
+;
+
+--Otra tabla general mas completa para mi referencia
+SELECT id_fac, fecha, id_cli, nombre, ID_pro, nombre_producto, valor_unitario, cantidad, valor_sin_iva, iva, valor_total
+FROM (
+        SELECT *, valor_unitario*cantidad AS valor_sin_iva, valor_unitario*cantidad*0.19 AS IVA, (valor_unitario*cantidad + valor_unitario*cantidad*0.19)  AS valor_total
+        FROM ((
+            SELECT *
+            FROM info_cliente i JOIN factura f ON i.ID_cli = f.id_cliente
+            )  AS cliente_factura
+            JOIN cantidades ON cantidades.id_factura = cliente_factura.ID_fac ) AS cliente_factura_cantidad
+            JOIN productos ON productos.ID_pro = cliente_factura_cantidad.id_producto
+    ) AS tablisima
+;
+
+--Que cliente realizo la compra mas cara?
+SELECT nombre, ID_fac, fecha, sum(valor_total) AS total
+FROM (
+        SELECT *, valor_unitario*cantidad AS valor_sin_iva, valor_unitario*cantidad*0.19 AS IVA, (valor_unitario*cantidad + valor_unitario*cantidad*0.19)  AS valor_total
+        FROM ((
+            SELECT *
+            FROM info_cliente i JOIN factura f ON i.ID_cli = f.id_cliente
+            )  AS cliente_factura
+            JOIN cantidades ON cantidades.id_factura = cliente_factura.ID_fac ) AS cliente_factura_cantidad
+            JOIN productos ON productos.ID_pro = cliente_factura_cantidad.id_producto
+    ) AS tablisima
+GROUP BY ID_fac, fecha, nombre
+ORDER BY total DESC
+LIMIT 1
+;
+
+
+--Que cliente pago sobre 100 de monto?
+SELECT *
+FROM (
+    SELECT nombre, sum(valor_total) AS total
+    FROM 
+        (
+        SELECT *, valor_unitario*cantidad AS valor_sin_iva, valor_unitario*cantidad*0.19 AS IVA, (valor_unitario*cantidad + valor_unitario*cantidad*0.19)  AS valor_total
+        FROM ((
+            SELECT *
+            FROM info_cliente i JOIN factura f ON i.ID_cli = f.id_cliente
+            )  AS cliente_factura
+            JOIN cantidades ON cantidades.id_factura = cliente_factura.ID_fac ) AS cliente_factura_cantidad
+            JOIN productos ON productos.ID_pro = cliente_factura_cantidad.id_producto
+        ) AS tablisima
+    GROUP BY nombre
+    ) AS resumen   
+WHERE total >100
+;
+
+--Cuantos clientes han comprado el producto 6?
+SELECT DISTINCT(nombre) --, ID_fac, fecha, id_pro, nombre_producto
+FROM (
+        SELECT *, valor_unitario*cantidad AS valor_sin_iva, valor_unitario*cantidad*0.19 AS IVA, (valor_unitario*cantidad + valor_unitario*cantidad*0.19)  AS valor_total
+        FROM ((
+            SELECT *
+            FROM info_cliente i JOIN factura f ON i.ID_cli = f.id_cliente
+            )  AS cliente_factura
+            JOIN cantidades ON cantidades.id_factura = cliente_factura.ID_fac ) AS cliente_factura_cantidad
+            JOIN productos ON productos.ID_pro = cliente_factura_cantidad.id_producto
+    ) AS tablisima
+WHERE id_pro =6
+;
